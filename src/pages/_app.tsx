ccd36046ea0d85cc619afc9d34990bad5c53e490
Arthur Assuncao <arthur.xx92@gmail.com>
2021-07-28 15:34:47 -0300

    feat: index defined

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,52 @@
-import type { AppProps } from 'next/app'
-import '../assets/styles/globals.scss'
+import universalLanguageDetect from "@unly/universal-language-detector";
+import get from "lodash.get";
+import NextCookies from "next-cookies";
+import NextApp from "next/app";
+import React from "react";
+import { FALLBACK_LANG, SUPPORTED_LANGUAGES } from "../utils/i18n";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+class App extends NextApp {
+  static async getInitialProps(props: any) {
+    const { ctx } = props;
+    const { req } = ctx;
+    const cookies = NextCookies(ctx); // Parses Next.js cookies in a universal way (server + client) - It's an object
+
+    // Universally detects the user's language
+    const lang = universalLanguageDetect({
+      supportedLanguages: SUPPORTED_LANGUAGES, // Whitelist of supported languages, will be used to filter out languages that aren't supported
+      fallbackLanguage: FALLBACK_LANG, // Fallback language in case the user's language cannot be resolved
+      acceptLanguageHeader: get(req, "headers.accept-language"), // Optional - Accept-language header will be used when resolving the language on the server side
+      serverCookies: cookies, // Optional - Cookie "i18next" takes precedence over navigator configuration (ex: "i18next: fr"), will only be used on the server side
+      errorHandler: (error, level, origin, context) => {
+        // Optional - Use you own logger here, Sentry, etc.
+        console.log("Custom error handler:");
+        console.error(error);
+      },
+    });
+    console.log("lang", lang);
+
+    // Calls page's `getInitialProps` and fills `appProps.pageProps` - XXX See https://nextjs.org/docs#custom-app
+    const appProps = await NextApp.getInitialProps(props);
+
+    appProps.pageProps = {
+      ...appProps.pageProps,
+      cookies, // Object containing all cookies
+      lang, // i.e: 'en'
+      isSSR: !!req,
+    };
+
+    return { ...appProps };
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+    const modifiedPageProps = {
+      ...pageProps,
+      router,
+    };
+
+    return <Component {...modifiedPageProps} />;
+  }
 }
-export default MyApp
+
+export default App;
---
+++ src/utils/i18n.ts
@@ -0,0 +1,5 @@
+export const FALLBACK_LANG = 'pt';
+export const SUPPORTED_LANGUAGES = [
+  'pt',
+  'en',
+];
